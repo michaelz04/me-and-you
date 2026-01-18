8c3a79e9544949ef7479461b8c3be6103e8915e4
Michael <michaelzhou20111@hotmail.com>
2026-01-18 11:29:35 -0500

    added Attack and Item buttons with corresponding functionality in the battle scene

--- scripts/battle.gd
+++ scripts/battle.gd
@@ -4,3 +4,9 @@ extends Node2D
 
 func _on_back_to_game_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
+
+func _on_attack_button_pressed():
+	print("Attack button pressed!")
+
+func _on_item_button_pressed():
+	print("Item button pressed!")
