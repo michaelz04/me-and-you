c80928b2e0098159fca8b935fe59dbb50092a2e1
Michael <michaelzhou20111@hotmail.com>
2026-01-18 11:45:09 -0500

    added Main Menu button and functionality to return to the main menu in the game scene

--- scripts/game.gd
+++ scripts/game.gd
@@ -2,3 +2,6 @@ extends Node2D
 
 func _on_enter_battle_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/battle.tscn")
+
+func _on_main_menu_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
