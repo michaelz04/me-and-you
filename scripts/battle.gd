c84154b72482aed46e43b1f75751f245c9c10ced
Michael <michaelzhou20111@hotmail.com>
2026-01-18 11:33:36 -0500

    added health display for player and enemy in battle scene with corresponding script functionality

--- scripts/battle.gd
+++ scripts/battle.gd
@@ -2,6 +2,21 @@ extends Node2D
 
 # Simple battle scene placeholder
 
+var player_hp: int = 100
+var player_max_hp: int = 100
+var enemy_hp: int = 100
+var enemy_max_hp: int = 100
+
+@onready var player_hp_label = $PlayerHPLabel
+@onready var enemy_hp_label = $EnemyHPLabel
+
+func _ready():
+	update_hp_display()
+
+func update_hp_display():
+	player_hp_label.text = "HP: %d / %d" % [player_hp, player_max_hp]
+	enemy_hp_label.text = "HP: %d / %d" % [enemy_hp, enemy_max_hp]
+
 func _on_back_to_game_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
 
